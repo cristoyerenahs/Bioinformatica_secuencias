7ab0511d1ffe1df77fc285ac86756171bbe8c6a0
cristoyerenahs <60819833+cristoyerenahs@users.noreply.github.com>
2020-02-25 22:07:18 -0600

    Delete genomicsalignments_prueba.R

--- genomicsalignments_prueba.R
+++
@@ -1,9 +0,0 @@
-install.packages("Bioconductor")
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install()
-library(GenomicAlignments)
-aln1_file <- system.file("extdata", "ex1.bam", package="Rsamtools")
-aln1 <- readGAlignments(aln1_file)
-aln1
-
